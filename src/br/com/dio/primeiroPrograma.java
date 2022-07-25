a6a19c6e4e5383c4ef421563cad28cf95dab87b3
deboraddarin <deboradarin@gmail.com>
2022-07-25 15:02:15 -0300

    update

--- src/br/com/dio/primeiroPrograma.java
+++ src/br/com/dio/primeiroPrograma.java
@@ -1,2 +1,16 @@
-package br.com.dio;public class primeiroPrograma {
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class primeiroPrograma {
+    public static void main(String[] args){
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+
+        /*int a=5;
+        int b = 3;
+        System.out.println("hello word"+(a+b));*/
+
+    }
 }
